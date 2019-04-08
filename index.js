5c09657827e317cd1f7fa04f72e5e3173b64e4ba
Kristijan Baro <baro.kristijan@gmail.com>
2019-04-08 15:00:23 +0200

    Files

---
+++ index.js
@@ -0,0 +1,26 @@
+//Comment
+        console.log('Hello World');
+
+        let name = 'Kristijan';
+        console.log(name);
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number(1name)
+//Cannot contain a space or hyphen (-)
+//Are case-sensitive
+        let firstName = 'Kristijan';
+        let lastName = 'Baro';
+
+//Const sluzi za upotrebu jednom, let za vise puta
+
+        let interestRate = 0.3;
+        interestRate = 1;
+        console.log(interestRate);
+
+//Tipovi podataka
+
+        let name = 'Ime'; //String
+        let age = 22; //Number
+        let isApproved = true; //Boolean (true or false)
+        let firstName = undefined;
+        let selectedColor = null;
